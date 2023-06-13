02164d660b00a149de8a386f031db859c111813c
noramari <noora@selinan.com>
2023-06-13 15:17:01 +0900

    Add MessageBubble

---
+++ FireChat/Components/MessageBubble.swift
@@ -0,0 +1,45 @@
+//
+//  MessageBubble.swift
+//  FireChat
+//
+//  Created by Noora Maeda on 2023/06/13.
+//
+
+import SwiftUI
+
+struct MessageBubble: View {
+    var message: Message
+
+    @State private var showTime = false
+
+    var body: some View {
+        VStack(alignment: message.received ? .leading : .trailing) {
+            HStack {
+                Text(message.text)
+                    .padding()
+                    .background(message.received ? Color("Gray") : Color("Peach"))
+                    .cornerRadius(25)
+            }
+            .frame(maxWidth: 300, alignment: message.received ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
+            }
+
+            if showTime {
+                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                    .padding(message.received ? .leading : .trailing, 25)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: message.received ? .leading : .trailing)
+        .padding(message.received ? .leading : .trailing)
+        .padding(.horizontal, 10)
+    }
+}
+
+struct MessageBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageBubble(message: Message(id: "1234", text: "Hello! How are you doing?", received: false, timestamp: Date()))
+    }
+}
