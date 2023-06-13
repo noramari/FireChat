133891f574702535cf56a9c252401dd74534064d
noramari <noora@selinan.com>
2023-06-13 14:49:12 +0900

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Noora Maeda on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
